2c7d3be1f40bb47f74abc7fe41741557cc8a61ac
Maria Eduarda Teodoro <141273429+madzsn@users.noreply.github.com>
2023-08-10 17:14:06 -0300

    Update SorteioMegaSenai.cpp

--- SorteioMegaSenai.cpp
+++ SorteioMegaSenai.cpp
@@ -1,60 +1,5 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
-
-int gerarNumeroAleatorio(int seed) {
-// Gere um número "pseudo-aleatório" usando aritmética modular
-int numero = (seed * 13 + 7) % 60 + 1;
-
-return numero;
-}
-
-
-int main () {
-
-    int contador = 6;
-    while (contador < 61) {
-        std::cout << contador << std::endl;
-        contador++;
-    }
-
-
-    setlocale(LC_ALL, "portuguese");
-   
-     // Gera um número secreto entre 1 e 100
-    int numeroSecreto = rand() % 60 + 1;
-    int tentativa, tentativas = 0;
-    
-    // Inicializa o gerador de números aleatórios
-    srand(static_cast<unsigned int>(time(0)));
-
-    
-    for (int i = 0; i < 60; i++) {
-       cout << numeroSecreto;
-    }
-
-
-
-    cout << "Bem vindo ao Mega Senai!" << endl;
-
-    do {
-    cout << "Digite um número de 1 a 60: ";
-    cin >> tentativa;
-    tentativas++;
-    
-    } while (tentativa != numeroSecreto);
-
-
-return 0;
-
-}
-
-
-
-// outro
-
-#include <iostream>
+//Alunas: Elen Vitória, Leticia, Maria Eduarda
+#include <iostream>    
 #include <cstdlib>
 #include <ctime>
 #include <vector>
@@ -62,50 +7,92 @@ return 0;
 
 using namespace std;
 
+
 int main() {
     srand(time(0));
-    
-    cout << "Bem-vindo ao Mega Senai!" << endl;
+        int vetorSize = 6; // Define o tamanho do vetor
+        int acertos = 0;
+
+            vector<int> numerosSorteados(vetorSize);
+
+            for (int i = 0; i < 6; i++) {
+        numerosSorteados[i] = rand() % 60 + 1;  // Gera número entre 1 e 60 aleatoriamente
+        numerosSorteados[i];
+        } 
+       cout << "Bem-vindo ao Mega Senai!" << endl; // Introdução ao jogo
+     
     
     int sorteios = 0;
     bool acertou = false;
 
     do {
-        vector<int> numerosSorteados;
         for (int i = 0; i < 6; i++) {
-            numerosSorteados.push_back(rand() % 60 + 1);
-        }
-        sort(numerosSorteados.begin(), numerosSorteados.end());
-
+        } 
+        
+    for (int i = 0; i < 6; i++) {
+         numerosSorteados[i];
+        } 
         cout << "Escolha 6 números distintos entre 1 e 60:" << endl;
         vector<int> numerosEscolhidos;
         for (int i = 0; i < 6; i++) {
+            
             int numero;
             cin >> numero;
             numerosEscolhidos.push_back(numero);
+        
+
+         if ( numero >= 61) {
+        cout << "O número digitado é maior que 60, tente novamente. " << endl;
+        break;
+         
+          } else if ( numero <= 0) {
+        cout << "O número digitado é menor que 1, tente novamente. " << endl;
+        break;
         }
+
+        
+
         sort(numerosEscolhidos.begin(), numerosEscolhidos.end());
 
-        int acertos = 0;
         for (int i = 0; i < 6; i++) {
             if (binary_search(numerosSorteados.begin(), numerosSorteados.end(), numerosEscolhidos[i])) {
                 acertos++;
-            }
-        }
 
-        cout << "Você acertou " << acertos << " números." << endl;
+                
+            }
+            
+        }
+        
         
         if (acertos == 6) {
             acertou = true;
             cout << "Parabéns! Você acertou todos os números em " << sorteios + 1 << " sorteios." << endl;
+
+            
+        } 
+         
+    } 
+        for (int i = 0; i < 6; i++) {
+         cout<<numerosSorteados[i]<<" " <<endl;
+                  cout<<numerosSorteados[i]<<" " <<endl;
+
+        } 
+    cout << "Você acertou " << acertos << " números." << endl;
+ char confirmacao;
+        std::cout << "\nVocê deseja continuar jogando? (S/N) ";
+        std::cin >> confirmacao;
+
+       if (toupper(confirmacao) == 'S') {
+            
         } else {
-            cout << "Tente novamente." << endl;
-            sorteios++;
-        }
-
-    } while (!acertou);
-
-    cout << "Obrigado por jogar o Mega Senai!" << endl;
-
+            std::cout << "Obrigado por jogar. " << std::endl;
+        } break;
+    }
+       while (!acertou);
+      
+    
     return 0;
-}
+
+        }            
+
+
