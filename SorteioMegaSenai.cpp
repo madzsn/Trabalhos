fea9c34ac9dbb9d77d6f3472b8a7d3b628f4fc5d
Maria Eduarda Teodoro <141273429+madzsn@users.noreply.github.com>
2023-08-10 01:10:15 -0300

    Update SorteioMegaSenai.cpp

--- SorteioMegaSenai.cpp
+++ SorteioMegaSenai.cpp
@@ -49,3 +49,63 @@ int main () {
 return 0;
 
 }
+
+
+
+// outro
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+    
+    cout << "Bem-vindo ao Mega Senai!" << endl;
+    
+    int sorteios = 0;
+    bool acertou = false;
+
+    do {
+        vector<int> numerosSorteados;
+        for (int i = 0; i < 6; i++) {
+            numerosSorteados.push_back(rand() % 60 + 1);
+        }
+        sort(numerosSorteados.begin(), numerosSorteados.end());
+
+        cout << "Escolha 6 números distintos entre 1 e 60:" << endl;
+        vector<int> numerosEscolhidos;
+        for (int i = 0; i < 6; i++) {
+            int numero;
+            cin >> numero;
+            numerosEscolhidos.push_back(numero);
+        }
+        sort(numerosEscolhidos.begin(), numerosEscolhidos.end());
+
+        int acertos = 0;
+        for (int i = 0; i < 6; i++) {
+            if (binary_search(numerosSorteados.begin(), numerosSorteados.end(), numerosEscolhidos[i])) {
+                acertos++;
+            }
+        }
+
+        cout << "Você acertou " << acertos << " números." << endl;
+        
+        if (acertos == 6) {
+            acertou = true;
+            cout << "Parabéns! Você acertou todos os números em " << sorteios + 1 << " sorteios." << endl;
+        } else {
+            cout << "Tente novamente." << endl;
+            sorteios++;
+        }
+
+    } while (!acertou);
+
+    cout << "Obrigado por jogar o Mega Senai!" << endl;
+
+    return 0;
+}
